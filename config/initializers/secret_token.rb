5da8627c95b065fed365c9b6b4b6af58c4b6a1c1
Steve Schaming <stevenschaming@gmail.com>
2014-01-12 13:45:25 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Othello::Application.config.secret_key_base = '8e62b4b94f8f043e27254019a545660af28812dfb0e67c5e713b03d6f1f767b39d6ad4c1eaf29b50e989a2dad75f25669ad9211d3010a66c1c7551999c2a8bf3'
